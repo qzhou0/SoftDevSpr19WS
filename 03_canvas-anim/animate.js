e9c487ef87b034a8a00d66eb73f2996b735d7a6c
Qian Zhou <33487776+qzhou0@users.noreply.github.com>
2019-02-05 16:50:52 -0500

    Update animate.js

--- 03_canvas-anim/animate.js
+++ 03_canvas-anim/animate.js
@@ -1,6 +1,6 @@
-1//  -- Ray Onishi, Qian Zhou
+// Smooth Sailing -- Ray Onishi, Qian Zhou
 // SoftDev2 pd7
-// K #03: 
+// K03 -- They lock us in the tower whenever we get caught
 // 2019-02-06
 
 
@@ -20,26 +20,31 @@ var radius=0;
 var growing=true;
 
 var draw=function(){
+    // Clears the canvas before generating every new frame
     clear();
+    // Checking if the circle reaches the edge of the canvas
+    // If it is at the edge, growing is set to false to make it shrink
     if (radius==c.width/2){
-	growing=false;
+	     growing=false;
     }
+    // Creating the circle
     ctx.beginPath();
     ctx.fillStyle="#00ffff";
     ctx.arc(c.width/2, c.height/2,radius,0,2*Math.PI);
     ctx.stroke();
     ctx.fill();
+    // If the circle is growing, increase radius by 1 on every frame
     if (growing){
-	radius+=1;
-	
+	     radius+=1;
     }
+    // Otherwise decrease the radius by 1 on every frame
     else{
-	radius-=1;
-	if (radius==0){
-	    growing=true;
-	}
+	     radius-=1;
+	      if (radius==0){
+	         growing=true;
+	      }
     }
-    
+
     requestID=window.requestAnimationFrame(draw);
     //console.log(requestID);
 };
@@ -52,13 +57,7 @@ circle.addEventListener( "click" , function(){
 });
 
 var stopIt=function(){
-    
     window.cancelAnimationFrame(requestID);
-    
 };
 
 stop.addEventListener('click',stopIt);
-
-
-
-
