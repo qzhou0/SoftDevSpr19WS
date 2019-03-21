20dac2e1fff384d29c2ea18d79a9c545ed00fdc1
Qian Zhou <qzhou@stuy.edu>
2019-03-20 22:04:07 -0400

    d3.tsv shoudl work...?

--- 13_svg/index.js
+++ 13_svg/index.js
@@ -1,49 +1,53 @@
 
 
-var data = [4, 8, 15, 16, 23, 42];
+// var data = [4, 8, 15, 16, 23, 42]; 
 var width = 420;var barHeight = 20;
 
-/*
 
 
 
 
 
-var x = d3.scaleLinear().range([0,width]);*/
 
-var x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, 10*d3.max(data)]);//why use 420, not this?// 420]);
+var x = d3.scaleLinear().range([0,width]);
+
+// var x = d3.scaleLinear()
+//     .domain([0, d3.max(data)])
+//     .range([0, 10*d3.max(data)]);//why use 420, not this?// 420]);
 
 
 
 
 
 var chart=d3.select(".chart")
-    .attr("width",width)
-    .attr("height",barHeight*data.length);
+    .attr("width",width);
+//     .attr("height",barHeight*data.length);
 
 
-/* don't know how to download data.tsv, w.o http
 
-d3.tsv("data.tsv",type,function(error, data){//asynchronous downloading
+
+//d3.tsv("data.tsv",type,function(error, data){//asynchronous downloading
+d3.tsv("data.tsv", type).then(function(data) {
     x.domain([0,d3.max(data,function(d){return d.value;})]);
+    console.log(data.length);
     chart.attr("height",barHeight * data.length);
-
+    console.log(data);
+    
+    
     var bar = chart.selectAll("g")
 	.data(data)
       .enter().append("g")
 	.attr("transform",function(d, i){return "translate(0,"+ i * barHeight + ")"; });
 
     bar.append("rect")
-	.attr("width",x)
+	.attr("width",(d)=>{return x(d.value);})//x takes d.value
 	.attr("height",barHeight-1);
 
     bar.append("text")
-	.attr("x", function(d) { return x(d) - 3; })
+	.attr("x", function(d) { return x(d.value) - 3; })
 	.attr("y", barHeight / 2)
 	.attr("dy", ".35em")
-	.text(function(d) { return d; });
+	.text(function(d) { return d.value; });
     
 });//close d3.tsv
 
@@ -51,26 +55,26 @@ function type(d){
     d.value=+d.value;// coerce to number
     return d
 }
-*/
 
 
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform",function(d, i){return "translate(0,"+ i * barHeight + ")"; });
+
+// var bar = chart.selectAll("g")
+//     .data(data)
+//   .enter().append("g")
+//     .attr("transform",function(d, i){return "translate(0,"+ i * barHeight + ")"; });
 
 //Since there is exactly one rect and one text element per g element, we can append these elements directly to the g, without needing additional data joins. Data joins are only needed when creating a variable number of children based on data; here we are appending just one child per parent. --tutorial
 
 
-bar.append("rect")
-    .attr("width",x)
-    .attr("height",barHeight-1);
+// bar.append("rect")
+//     .attr("width",x)
+//     .attr("height",barHeight-1);
 
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+// bar.append("text")
+//     .attr("x", function(d) { return x(d) - 3; })
+//     .attr("y", barHeight / 2)
+//     .attr("dy", ".35em")
+//     .text(function(d) { return d; });
 
 
 
